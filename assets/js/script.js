3f0f64e1f007e3d0b8cac7f0ad433e53caeebea6
aznjp <jpark103193@gmail.com>
2020-09-05 16:55:43 -0400

    the timeMatch function was added to assign classes to individual divs so that they may have color coding for the time that has passed and new comments were added to the html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,25 @@
+var hour = moment().hours();
+
+
 // This is a function used to call up the current date using moment.js 
 function dateTimer() {
     $("#currentDay").text(moment().format('MMMM Do YYYY'));
+};
+
+/*Color coding schedule dependant upon whether the id numbers (which match time divs) are
+equivalent to the actual current hour via the variable at top and css stylings*/
+function timeMatch() {
+    $(".description").each(function() {
+
+        var rowHour = $(this).attr("id");
+        var rowNumber = parseInt(rowHour);
+
+        if (rowNumber == hour) {
+            $(this).addClass("present");
+        } else if (rowNumber < hour) {
+            $(this).addClass("past");
+        } else {
+            $(this).addClass("future");
+        };
+    });
 };
